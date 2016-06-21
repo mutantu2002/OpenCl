d7b82c9dbcf62a04f7a5d3bbabfa5734cffc8fd4
mutantu2002 <mutantu2002@yahoo.com>
2016-06-21 18:34:54 +0300

    Gradient clustering

---
+++ src/main/resources/dot/GradientClustering.c
@@ -0,0 +1,88 @@
+__kernel void train(__global const float *perceptrons, __global const short *images, __global float *updates )
+{
+	int imagesOffset = get_global_id(0)*IMAGE_SIZE*BATCH_ITEMS;
+	int updatesOffset = get_global_id(0)*NO_PERCEPTRONS*(IMAGE_SIZE+1);
+	int labelsOffset = get_global_id(0)*BATCH_ITEMS;
+
+	int perceptron;
+	float sum;
+	float maxForExp;
+	int offset;
+	int batch;
+	float activation[NO_CLASSES];
+	for(batch=0;batch<BATCH_ITEMS;batch++)
+	{
+		maxForExp = -FLT_MAX;
+		for(perceptron=0;perceptron<NO_CLASSES;perceptron++)
+		{
+			sum=0;
+			for(offset=0;offset<IMAGE_SIZE;offset++)
+			{
+				sum+=perceptrons[perceptron*(IMAGE_SIZE+1)+offset]*images[imagesOffset+batch*IMAGE_SIZE+offset]/128.;
+			}
+			sum+=perceptrons[perceptron*(IMAGE_SIZE+1)+IMAGE_SIZE];
+			if(sum>maxForExp)
+			{
+				maxForExp=sum;
+			}
+			activation[perceptron]=sum;
+		}
+		for(perceptron=0;perceptron<NO_CLASSES;perceptron++)
+		{
+			activation[perceptron]-=maxForExp;
+			activation[perceptron]=exp(activation[perceptron]);
+		}
+
+		sum=0;
+		for(perceptron=0;perceptron<NO_CLASSES;perceptron++)
+		{
+			sum+=activation[perceptron];
+		}
+
+		for(perceptron=0;perceptron<NO_CLASSES;perceptron++)
+		{
+			if(perceptron==labels[labelsOffset+batch])
+			{
+				activation[perceptron]=activation[perceptron]/sum-1;
+			}else
+			{
+				activation[perceptron]/=sum;
+			}
+		}
+
+		for(perceptron=0;perceptron<NO_CLASSES;perceptron++)
+		{
+			for(offset=0;offset<IMAGE_SIZE;offset++)
+			{
+				updates[updatesOffset+perceptron*(IMAGE_SIZE+1)+offset]+=-images[imagesOffset+batch*IMAGE_SIZE+offset]*activation[perceptron]/128.;
+			}
+			updates[updatesOffset+perceptron*(IMAGE_SIZE+1)+IMAGE_SIZE]+=-activation[perceptron];
+		}
+	}
+}
+__kernel void test(__global const float *perceptrons, __global const short *images, __global short *labels )
+{
+	int imagesOffset = get_global_id(0)*IMAGE_SIZE;
+	int labelsOffset = get_global_id(0);
+	float sum;
+	int perceptron;
+	int offset;
+	float max=-FLT_MAX;
+	int maxLabel;
+
+	for(perceptron=0;perceptron<NO_CLASSES;perceptron++)
+	{
+		sum=0;
+		for(offset=0;offset<IMAGE_SIZE;offset++)
+		{
+			sum+=perceptrons[perceptron*(IMAGE_SIZE+1)+offset]*images[imagesOffset+offset]/128.;
+		}
+		sum+=perceptrons[perceptron*(IMAGE_SIZE+1)+IMAGE_SIZE];
+		if(sum>max)
+		{
+			max=sum;
+			maxLabel=perceptron;
+		}
+	}
+	labels[labelsOffset]=maxLabel;
+}
